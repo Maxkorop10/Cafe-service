b5bcfafd114efda609846d8cde73b82b4c216f35
Karpenoid <mxkarpen@gmail.com>
2025-05-07 20:21:10 +0300

    Added cafe location via Google Maps.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,21 @@
 import type { Metadata } from "next";
 import { CafeInfo } from "@/components/cafe-info";
 import { InteriorList } from "@/components/interior-list";
+import {GoogleMap} from "@/components/google-map";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
 export default function Home() {
-  // const { data: session, status } = useSession();
-  //
-  // useEffect(() => {
-  //     if (status === "authenticated") {
-  //         console.log("User role:", session?.user?.role);
-  //     }
-  // }, [session, status]);
-
   return (
     <div className="py-4.5 px-5 w-full">
       <div className="flex justify-center items-start gap-6">
         <InteriorList />
-        <CafeInfo />
+          <div className="grid grid-cols-1 gap-4">
+              <CafeInfo />
+              <GoogleMap />
+          </div>
       </div>
     </div>
   );
--- src/components/cafe-map-location/cafe-map-location.tsx
+++
@@ -1,3 +0,0 @@
-export function CafeMapLocation() {
-  return <div></div>;
-}
